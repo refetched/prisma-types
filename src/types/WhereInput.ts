a56574cd1eca6136eb5b9ba22f3934cde7e181ff
rconrardy <conrardy@umich.edu>
2023-09-03 23:29:34 -0400

    [v1.0.6] Update where input.

--- src/types/WhereInput.ts
+++ src/types/WhereInput.ts
@@ -1,7 +1,7 @@
 import { Keys } from '@refetched/core-types';
 
-export type WhereInput<T extends object> =
-  | { [K in Keys<T>]?: unknown }
-  | { AND?: WhereInput<T>[] }
-  | { OR?: WhereInput<T>[] }
-  | { NOT?: WhereInput<T>[] };
+export type WhereInput<T extends object> = { [K in Keys<T>]?: unknown } & {
+  AND?: WhereInput<T>[];
+  OR?: WhereInput<T>[];
+  NOT?: WhereInput<T>[];
+};
